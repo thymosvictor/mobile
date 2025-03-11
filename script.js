dfd9e42172ae86357ebd8aa264ccd7c378787ffe
thymosvictor <166143287+thymosvictor@users.noreply.github.com>
2025-03-11 15:01:20 +0000

    ajustar tela der login

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const searchInput = document.getElementById("search");
-    const eventCards = document.querySelectorAll(".card");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const searchInput = document.getElementById("search");
+//     const eventCards = document.querySelectorAll(".card");
 
-    -
-    searchInput.addEventListener("input", () => {
-        const searchTerm = searchInput.value.toLowerCase();
+//     -
+//     searchInput.addEventListener("input", () => {
+//         const searchTerm = searchInput.value.toLowerCase();
 
-        eventCards.forEach(card => {
-            const eventName = card.querySelector("h3").textContent.toLowerCase();
-            const eventLocation = card.querySelector("p").textContent.toLowerCase();
-            const eventDate = card.querySelector("span").textContent.toLowerCase();
+//         eventCards.forEach(card => {
+//             const eventName = card.querySelector("h3").textContent.toLowerCase();
+//             const eventLocation = card.querySelector("p").textContent.toLowerCase();
+//             const eventDate = card.querySelector("span").textContent.toLowerCase();
 
-            if (eventName.includes(searchTerm) || eventLocation.includes(searchTerm) || eventDate.includes(searchTerm)) {
-                card.style.display = "block";
-            } else {
-                card.style.display = "none";
-            }
-        });
-    });
+//             if (eventName.includes(searchTerm) || eventLocation.includes(searchTerm) || eventDate.includes(searchTerm)) {
+//                 card.style.display = "block";
+//             } else {
+//                 card.style.display = "none";
+//             }
+//         });
+//     });
 
   
-    const buttons = document.querySelectorAll("footer .buttons button");
+//     const buttons = document.querySelectorAll("footer .buttons button");
 
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert(`Você clicou em: ${button.textContent.trim()}`);
-        });
-    });
-});
+//     buttons.forEach(button => {
+//         button.addEventListener("click", () => {
+//             alert(`Você clicou em: ${button.textContent.trim()}`);
+//         });
+//     });
+// });
