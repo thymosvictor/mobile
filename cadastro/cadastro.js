8d9e5e51649aff59e393c53c03423401f9f97e63
thymosvictor <166143287+thymosvictor@users.noreply.github.com>
2025-03-13 12:52:15 +0000

    terminar tela de cadastro

---
+++ cadastro/cadastro.js
@@ -0,0 +1,16 @@
+const estados = [
+    "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+    "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul",
+    "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro",
+    "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina",
+    "São Paulo", "Sergipe", "Tocantins"
+];
+
+const selectEstados = document.getElementById("estados");
+
+estados.forEach(estado => {
+    const option = document.createElement("option");
+    option.value = estado;
+    option.textContent = estado;
+    selectEstados.appendChild(option);
+});
